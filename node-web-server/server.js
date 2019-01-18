bedbc7bdb87988f75cf682577cc4ae169ac7df47
Antonio Rodriguez <antorodriguez@gmail.com>
2019-01-18 15:18:48 -0300

    Added templates hbs

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,25 +1,37 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
-
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public/'))
 
-
-app.get('/', (req, res)=> {
- res.send('Hello Express!');
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
 })
 
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res)=> {
+    res.render('home.hbs', {
+        pageTitle: "Welcome page",
+        userName: 'Andrew', 
+    });
+   });
 
 app.get('/about', (req, res)=> {
-    res.send('About page!');
-   })
-
+    res.render('about.hbs', {
+        pageTitle: "About page",
+    });
+   });
 
 app.get('/bad', (req, res)=> {
     res.send({
         errorMessage: 'Unable to handle request'
     });
-   })
+   });
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')

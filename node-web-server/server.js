604b3d6573ddf1bc3be1632966e2ee8d9a38391f
Antonio Rodriguez <antorodriguez@gmail.com>
2019-01-18 21:15:06 -0300

    removes under maintenance middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public/'))
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = (`${now}: ${req.method} ${req.url}`);
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err){
+            console.log('Unable to append to log file')
+        }
+    })
+    next();
+});
+
+// to avoid continue moving into the site just avoid to call next
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// })
+
+app.use(express.static(__dirname + '/public/'));
 
 hbs.registerHelper('getCurrentYear', ()=>{
     return new Date().getFullYear();

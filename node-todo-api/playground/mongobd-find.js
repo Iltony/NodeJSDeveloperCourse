1992155caee7a03ecf1f51f5a97c46f77e1ecc78
Antonio Rodriguez <antorodriguez@gmail.com>
2019-02-19 00:19:25 -0300

    Add find with users

--- node-todo-api/playground/mongobd-find.js
+++ node-todo-api/playground/mongobd-find.js
@@ -21,14 +21,25 @@ MongoClient.connect('mongodb://testtodoapp:tesTtodOapp1230.6+@ds247674.mlab.com:
     // })
 
 
-    db.collection('Todos').find().count().then(
-        (count)=> {
-            console.log('Todos');
-            console.log(`Todos count ${count}`)
+    // db.collection('Todos').find().count().then(
+    //     (count)=> {
+    //         console.log('Todos');
+    //         console.log(`Todos count ${count}`)
+    //     }
+    // , (err) => {
+    //     console.log('Unable to fetch todos', err)
+    // })
+
+    
+    db.collection('Users').find({name: 'Antonio'}).toArray().then(
+        (docs) => {
+            console.log(JSON.stringify(docs, undefined, 2));
         }
     , (err) => {
-        console.log('Unable to fetch todos', err)
+        console.log('Unable to fetch users', err)
     })
+
+    
 
     // client.close();
 });

b237541c77b5bc2d08b523f75b84d6c3bcc565c7
Antonio Rodriguez <antorodriguez@gmail.com>
2019-02-19 14:20:34 -0300

    Add user schema to test mongoose validations. L71

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -6,31 +6,54 @@ mongoose.connect('mongodb://testtodoapp:tesTtodOapp1230.6+@ds247674.mlab.com:476
 var Todo = mongoose.model('Todo', 
 {
 	text: {
-		type: String
+		type: String,
+		required: true,
+		minlength: 1, // min length required, if empty or space the validation fails
+		trim: true //removes spaces in the value
 	}, completed:{
-		type: Boolean
+		type: Boolean,
+		default: false
 	}, completedAt:{
-		type: Number
+		type: Number,
+		default: null
 	} 
 });
 
 
-var newTodo = new Todo({
-	text: 'Cook dinner',
+// var newTodo = new Todo({
+// 	text: 'Cook dinner',
+// })
+
+// newTodo.save().then(
+// 	(doc)=>console.log('saved todo', doc),
+// 	(e) => console.log('Unable to save todo')	
+// );
+
+// var otherTodo = new Todo({
+// 	text: 'Cook dinner 2',
+// 	completed: true,
+// 	completedAt: 123
+// })
+
+// otherTodo.save().then(
+// 	(doc)=>console.log(JSON.stringify(doc, undefined, 2)),
+// 	(e) => console.log('Unable to save todo')	
+// );
+
+var User = mongoose.model('User', {
+	email: {
+		type: String, 
+		require: true,
+		trim: true,
+		minlength: 1
+	}
+});
+
+var user = new User({
+	email: "antonioantonio@gmail.com "	
 })
 
-newTodo.save().then(
-	(doc)=>console.log('saved todo', doc),
-	(e) => console.log('Unable to save todo')	
-);
-
-var otherTodo = new Todo({
-	text: 'Cook dinner 2',
-	completed: true,
-	completedAt: 123
-})
-
-otherTodo.save().then(
-	(doc)=>console.log(JSON.stringify(doc, undefined, 2)),
-	(e) => console.log('Unable to save todo')	
+user.save().then(
+	(doc)=> console.log(JSON.stringify(doc)),
+	() => console.log('Unable to save user')	
 );

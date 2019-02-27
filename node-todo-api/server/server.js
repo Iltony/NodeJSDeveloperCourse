d8dcea77dfef00309fc039c5900caaadfba4ee0b
Antonio Rodriguez <antorodriguez@gmail.com>
2019-02-27 18:18:00 -0300

    temporary commit

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -31,6 +31,28 @@ app.get('/todos', (req, res) => {
 	})
 	});
 
+app.get('/todos/:id', (req, res) => {
+		var todoId = req.params.id;
+
+		if(!ObjectId.isValid(todoId)){
+			console.log('Todo ID is not valid')
+		}
+
+		Todo.findById({
+			_id: todoId   
+		}).then((todo) => {
+			
+			if (!todo){
+				console.log('ID not found', todoId);
+				res.status(404).send({});
+			}
+
+			res.send(JSON.stringify(todo, undefined, 2));
+		}).catch(err => {
+			res.status(400).send(err);
+		})
+	});
+
 app.listen(3000, ()=> {
 	console.log('Started on port 3000');
 })
